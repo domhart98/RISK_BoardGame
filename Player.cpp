f0e9fc13d176c4da58773a58f4e134fde604bb14
domhart98 <domhart98@hotmail.com>
2022-08-08 13:21:45 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,174 +1,188 @@
-
 #include "Map.h"
 #include "Cards.h"
 #include "Player.h"
 
-
-// COMP 345
-// Player class implementation by Shane Fournier, February 13 2022.
-
-
-	
-        // default constructor
-		Player::Player(){
+// default constructor
+Player::Player(){
 		  
-		   owned;
-		   hand;
-		   numArmies = 0;
-		   name = "";
+	owned;
+	hand;
+	numArmies = 0;
+	name = "";
 
-		}
-		// parameterized constructor
-		Player::Player(string name){
+}
+
+// parameterized constructor
+Player::Player(string name){
 		  
-		   this->name = name;
-		   owned;
-		   hand;
-		   numArmies = 0;
+	this->name = name;
+	owned;
+	hand;
+	numArmies = 0;
 
-		}
-		// copy constructor
-		Player::Player(Player &plr)
-		{
-			owned = plr.owned;
-			hand = plr.hand;
-			numArmies = plr.numArmies;
+}
 
-		}
-		// destructor
-		Player::~Player()
-		{
-			for (Territory* owned : owned) {
-				delete owned;
-			}
+// copy constructor
+Player::Player(Player &plr){
+
+	owned = plr.owned;
+	hand = plr.hand;
+	numArmies = plr.numArmies;
+
+}
+
+// destructor
+Player::~Player(){
+
+	for (Territory* owned : owned) {
+		delete owned;
+	}
 		       
-			for (Card* card : this->hand) {
-				delete card;
-			}
+	for (Card* card : this->hand) {
+		delete card;
+	}
 
-            owned.clear();
-			hand.clear();
+    owned.clear();
+	hand.clear();
 			
-		}
+}
 		
-		vector<Territory*> Player::toAttack()
-		// Returns adjacent territories, that are not already owned by the player.
-		// Iterates through each owned territory as listed, checks adjacent territories for ownership, then adds if not owned by player.
-		{
-			vector <Territory*> t;
-		    for(Territory* terr : this->owned){
+// Returns adjacent territories, that are not already owned by the player.
+// Iterates through each owned territory as listed, checks adjacent territories for ownership, then adds if not owned by player.
+vector<Territory*> Player::toAttack(){
+			
+	vector <Territory*> t;
+	//Add all adjacent territories to vector
+	for(Territory* terr : this->owned){
 		    	
-		    	for (Territory* adjTerr: terr->adjacentTerritories)
-		    	{
+		for (Territory* adjTerr: terr->adjacentTerritories) 
+		{
 		   
-		    	    if (terr->name.compare(adjTerr->name) != 0)
-		    	    {
-						t.push_back(adjTerr);
-		    	    }   
+		    t.push_back(adjTerr);   
 		    		 
-				}
+		}
 		    	
-			}
-			sort(t.begin(), t.end());
-			vector<Territory*>::iterator lastUniqueTerritory;
-			lastUniqueTerritory = unique(t.begin(), t.end());
-			t.erase(lastUniqueTerritory, t.end());
+	}
 
-			cout << "Adjacent Territories: " << endl;
-			for (Territory* terr : t) {
-				cout << terr->name << " " << terr->armyNumber << ", ";
-			}
-			cout << endl;
-			return t;
-		}
-		
-		vector<Territory*> Player::toDefend()
-		// Prints and then returns owned territories
+	//removes all owned territories from adjacent territories vector
+	for (Territory* terr : this->owned) {
+
+		for (Territory* adjTerr : t)
 		{
-			cout << "Owned Territories: " << endl;
-				for (Territory* terr : this->owned) {
-					cout << terr->name << ", ";
-				}
-			cout << endl;
-			return owned;
+			if(terr->name == adjTerr->name)
+			t.push_back(adjTerr);
+
 		}
 
-		//t1 is the attacking territory, t2 is the defending territory. Roll to resolve, based on number of armies used.
-		void Player::attack(Territory* t1, Territory* t2) {
+	}
+
+	//remove duplicate territories from adjacent territories vector
+	sort(t.begin(), t.end());
+	vector<Territory*>::iterator lastUniqueTerritory;
+	lastUniqueTerritory = unique(t.begin(), t.end());
+	t.erase(lastUniqueTerritory, t.end());
+
+	//Display adjacent territories
+	cout << "Adjacent Territories: " << endl;
+	for (Territory* terr : t) {
+		cout << terr->name << " Defending Armies: " << terr->armyNumber << " || ";
+	}
+	cout << endl;
+	return t;
+
+}
+	
+// Prints and then returns owned territories
+vector<Territory*> Player::toDefend(){
+
+	cout << "Owned Territories: " << endl;
+		for (Territory* terr : this->owned) {
+			cout << terr->name << ", ";
+		}
+	cout << endl;
+	return owned;
+
+}
+
+//t1 is the attacking territory, t2 is the defending territory. Roll to resolve, based on number of armies used.
+void Player::attack(Territory* t1, Territory* t2) {
 			
-			if (t1->armyNumber < 2) {
-				cout << "Insufficient armies to attack from this territory." << endl;
-				return;
-			}
-			cout << "How many armies to attack with(1~3)?: ";
-			int attackingArmies;
-			cin >> attackingArmies;
+	if (t1->armyNumber < 2) {
+		cout << "Insufficient armies to attack from this territory." << endl;
+		return;
+	}
+	cout << "How many armies to attack with(1~3)?: ";
+	int attackingArmies;
+	cin >> attackingArmies;
 
-			cout << "How many armies to defend with(1~2)?: ";
-			int defendingArmies;
-			cin >> defendingArmies;
+	cout << "How many armies to defend with(1~2)?: ";
+	int defendingArmies;
+	cin >> defendingArmies;
 
-			//One army must remain on the attacking territory. 
-			if (attackingArmies < t1->armyNumber && defendingArmies <= t2->armyNumber) {
-				int attackRolls[3];
-				int defendRolls[2];
+	//One army must remain on the attacking territory. 
+	if (attackingArmies < t1->armyNumber && defendingArmies <= t2->armyNumber) {
+		int attackRolls[3];
+		int defendRolls[2];
 
-				for (int i = 0; i < attackingArmies; i++) {
-					attackRolls[i] = rand() % 6 + 1;
-				}
-				for (int i = 0; i < defendingArmies; i++) {
-					defendRolls[i] = rand() % 6 + 1;
-				}
-
-				sort(attackRolls, attackRolls + 3, greater<int>());
-				sort(defendRolls, defendRolls + 2, greater<int>());
-
-				for (int i = 0; i < defendingArmies; i++) {
-					if (attackRolls[i] > defendRolls[i]) {
-						t2->armyNumber--;
-						if (t2->armyNumber == 0) {
-							t2->owner = t1->owner;
-							t1->armyNumber--;
-							t2->armyNumber++;
-							return;
-						}
-					}
-					else {
-						t1->armyNumber--;
-					}
-				}
-			}
-			return;
+		for (int i = 0; i < attackingArmies; i++) {
+			attackRolls[i] = rand() % 6 + 1;
+		}
+		for (int i = 0; i < defendingArmies; i++) {
+			defendRolls[i] = rand() % 6 + 1;
 		}
 
-	    std::ostream& operator<<(std::ostream& out, const Player& plr)
-	   // Iterates through owned territories, and lists cards and orders.
-		{
-			out << "Owned territories: "; 
-			for (Territory* terr : plr.owned) {
-				out << *terr;
+		sort(attackRolls, attackRolls + 3, greater<int>());
+		sort(defendRolls, defendRolls + 2, greater<int>());
+
+		for (int i = 0; i < defendingArmies; i++) {
+			if (attackRolls[i] > defendRolls[i]) {
+				t2->armyNumber--;
+				if (t2->armyNumber == 0) {
+					t2->owner = t1->owner;
+					t1->armyNumber--;
+					t2->armyNumber++;
+					return;
+				}
 			}
-			
-			out << endl;
-
-			out << "Cards in hand: ";
-			for (Card* card : plr.hand) {
-				out << *card;
+			else {
+				t1->armyNumber--;
 			}
-
-			out << endl;
-			
-			return out;
 		}
-		
-		void Player::operator=(Player & plr)
-		// assignment operator.
-		{
-			owned = plr.owned;
-			hand = plr.hand;
-			
-		}
-		
+	}
+	return;
 
+}
+
+// ostream overload
+std::ostream& operator<<(std::ostream& out, const Player& plr){
+
+	out << "Player name: " << plr.name << endl;
+	
+	out << "Owned territories: "; 
+	for (Territory* terr : plr.owned) {
+		out << *terr;
+	}
+			
+	out << endl;
+
+	out << "Cards in hand: ";
+	for (Card* card : plr.hand) {
+		out << *card;
+	}
+
+	out << endl;
+			
+	return out;
+
+}
+
+// assignment operator.
+void Player::operator=(Player & plr){
+	
+	name = plr.name;
+	owned = plr.owned;
+	hand = plr.hand;
+			
+}
 
 
