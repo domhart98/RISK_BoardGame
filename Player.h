12cb043f15e3bee58f91e61fbda508081076a883
domhart98 <domhart98@hotmail.com>
2022-08-08 13:18:32 -0400

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,16 +3,12 @@
 #include <vector>
 #include <algorithm>
 
-class OrdersList;
 class Card;
 class Player;
-class Order;
 class Territory;
 
 class Player
 {
-	
-	// Components of the Player object: owned territories, a hand of cards, and a list of (non-card) orders
 
 	public:
 		vector<Territory*> owned;
@@ -20,20 +16,17 @@ class Player
 		int numArmies;
 		string name;
 		
-		// The Player API: default constructor, copy constructor, parameterized constructor and destructor
-  Player();
-  Player(string name);
-  Player(Player &plr);
-  ~Player();
-        // Lists of territories to attack or defend as derived from the Player's list of orders and cards
-  vector <Territory*> toAttack();
-  vector <Territory*> toDefend();
-  void attack(Territory* t1, Territory* t2);
-        // Adds an order to the Player's OrderList
-  void issueOrder(Order* ord);
-       // Prints out a listing of the player's components
-  friend std::ostream& operator<<(std::ostream & stream, const Player & plr);
-       // Assigns components to another Player object.
-  void operator=(Player & plr);
+		Player();
+		Player(string name);
+		Player(Player &plr);
+		~Player();
+        
+		vector <Territory*> toAttack();
+		vector <Territory*> toDefend();
+		void attack(Territory* t1, Territory* t2);
+        
+		friend std::ostream& operator<<(std::ostream & stream, const Player & plr);
+		void operator=(Player & plr);
+
 };
 
